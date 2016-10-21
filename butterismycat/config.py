3975d28e40b328f8b52741b149fc8892908e0731
Sean LaPlante <laplante.sean@gmail.com>
2016-10-21 09:41:45 -0400

    Adding mongo stuff

--- butterismycat/config.py
+++ butterismycat/config.py
@@ -1,4 +1,6 @@
-
+'''
+JSON/Mongo config file classes
+'''
 
 class ConfigFile(object):
     '''
@@ -9,12 +11,26 @@ class ConfigFile(object):
         pass
 
     def load(self):
+        '''
+        Load the config file from disk and parse JSON
+        '''
         pass
 
     def save(self):
+        '''
+        Save the config file as JSON to disk
+        '''
         pass
 
 class MongoConfig(ConfigFile):
+    '''
+    Handles config values for a mongo DB connection
+    '''
 
     def __init__(self, path, **kwargs):
         super(MongoConfig, self).__init__(path)
+        self.database_name = kwargs.get("db", "butterismycat")
+
+
+
+    
