f26f83082e68bd7607cd18827f9047133e457452
Sean LaPlante <laplante.sean@gmail.com>
2016-11-23 19:13:02 -0500

    Now actually doing some cool stuff.

--- comic/urls.py
+++ comic/urls.py
@@ -7,4 +7,6 @@ from . import views
 
 urlpatterns = [
     url(r'^$', views.index, name="index"),
+    url(r'^(?P<comic_id>[0-9]+)/$', views.comic, name="comic"),
+    url(r'archive/', views.archive, name="archive")
 ]
--- comic/views.py
+++ comic/views.py
@@ -2,12 +2,48 @@
 Views for comic app
 '''
 
-#from django.shortcuts import render
 from django.http import HttpResponse
+from django.template import loader
+from django.core.exceptions import ObjectDoesNotExist
+
+from .models import Comic
 
 def index(request):
     '''
     Home page
     '''
-    print("index(): ", request)
-    return HttpResponse("Hello world")
+    latest_comic = Comic.objects.latest('pub_date')
+    template = loader.get_template('comic/index.html')
+    context = {
+        'comic': latest_comic
+    }
+
+    return HttpResponse(template.render(context, request))
+
+def archive(request):
+    '''
+    Comic archive
+    '''
+    comics = Comic.objects.order_by('-pub_date') # Puts the newest first
+    template = loader.get_template('comic/archive.html')
+    context = {
+        'comics': comics
+    }
+
+    return HttpResponse(template.render(context, request))
+
+def comic(request, comic_id):
+    '''
+    Permalink for a specific comic
+    '''
+    try:
+        permalink_comic = Comic.objects.get(id=comic_id)
+    except ObjectDoesNotExist:
+        permalink_comic = None
+
+    template = loader.get_template('comic/index.html')
+    context = {
+        'comic': permalink_comic
+    }
+
+    return HttpResponse(template.render(context, request))
